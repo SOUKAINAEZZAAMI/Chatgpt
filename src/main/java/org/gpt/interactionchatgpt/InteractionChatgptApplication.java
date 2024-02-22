4455c823e6c516319a88e6b04d114e0803c7bc2f
SoukainaEzzaami <ezzaamisoukaina@gmail.com>
2024-02-22 22:26:38 +0100

    first code

---
+++ src/main/java/org/gpt/interactionchatgpt/InteractionChatgptApplication.java
@@ -0,0 +1,13 @@
+package org.gpt.interactionchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractionChatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractionChatgptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gpt/interactionchatgpt/InteractionChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gpt.interactionchatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractionChatgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
