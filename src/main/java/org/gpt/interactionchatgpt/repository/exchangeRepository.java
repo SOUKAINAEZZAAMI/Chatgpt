6e966b20d899c42df3445d8dddec20092c272daf
SoukainaEzzaami <ezzaamisoukaina@gmail.com>
2024-02-22 23:06:22 +0100

    code repository

---
+++ src/main/java/org/gpt/interactionchatgpt/repository/exchangeRepository.java
@@ -0,0 +1,10 @@
+package org.gpt.interactionchatgpt.repository;
+
+import org.gpt.interactionchatgpt.model.exchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface exchangeRepository  extends JpaRepository <exchange, Long>{
+
+
+}
+
---
+++ src/main/java/org/gpt/interactionchatgpt/repository/userRepository.java
@@ -0,0 +1,11 @@
+package org.gpt.interactionchatgpt.repository;
+
+import org.gpt.interactionchatgpt.model.user;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface userRepository  extends JpaRepository<user, Long> {
+
+
+
+}
+
